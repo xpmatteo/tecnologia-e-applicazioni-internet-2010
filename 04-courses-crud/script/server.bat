1677211632f0d6a30a95fa2a8245a308b33be07a
xpmatteo <vaccari@pobox.com>
2010-04-16 16:26:52 +0200

    add ant

--- 04-courses-crud/script/server.bat
+++ 04-courses-crud/script/server.bat
@@ -1,2 +1,15 @@
-ant war 
-java -jar lib/winstone-0.9.10.jar --warfile target/appname.war
+
+@echo off
+
+set JAVA_HOME=C:\Programmi\Java\jdk1.6.0_16
+set ANT_HOME=..\apache-ant-1.7.1
+
+call ..\apache-ant-1.7.1\bin\ant war
+
+if not exist target/courses.war (
+  goto end
+)
+
+java -jar lib/winstone-0.9.10.jar --warfile target/courses.war
+
+:end
